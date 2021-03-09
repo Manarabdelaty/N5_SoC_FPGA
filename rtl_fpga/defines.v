d32480009a6840ecb633e8cc4ebace61381f9a88
manarabdelaty <manarabdelatty@aucegypt.edu>
2021-03-09 16:56:29 +0200

    Update defines.v

--- rtl_fpga/defines.v
+++ rtl_fpga/defines.v
@@ -3,17 +3,23 @@
 
 `define USE_RESET_BTN
 
+`ifdef ICARUS_VERILOG 
+    `define ROM_FILE_PATH "../mem/test_rom_v.mem"
+    `define RAM_FILE_PATH "../mem/test_ram_v.mem"
+`else
+    `define ROM_FILE_PATH "test_rom_v.mem"
+    `define RAM_FILE_PATH "test_ram_v.mem"
+`endif
+
 `ifdef FETCH_FROM_ROM
     `define USE_ROM
     `define ROM_MEM_WORDS 8192
     `define INIT_ROM
     `define PC_START_ADDRESS 32'h4A000000
-    `define ROM_FILE_PATH "../mem/test_rom_v.mem"
 `else 
     `ifdef FETCH_FROM_RAM
         `define INIT_RAM
         `define PC_START_ADDRESS 32'h20000000
-        `define RAM_FILE_PATH "../mem/test_ram_v.mem"
     `else // Fetch from SPI Flash
         `define FETCH_FROM_FLASH
         `define PC_START_ADDRESS 32'h00000000
